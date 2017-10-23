e0698bb1c6f7c5f9794ff5873957f806f2222ca9
savegadmytro@gmail.com <339613Dimond>
2017-10-22 21:24:19 -0500

    ver. 3.0

--- app.js
+++ app.js
@@ -32,43 +32,36 @@ function ShoppingListCheckOffService(){
 
 var shoppingList = this;
 
-//list of items to buy
-shoppingList.itemsToBuy = [{name:"Milk", quantity:"2 bottles"},
-{name:"Bread", quantity:"2 pieces"},
-{name:"Juice", quantity:"1 liter"},
-{name:"Eggs", quantity:"30 pieces"},
-{name:"Sugar", quantity:"3 kilograms"}];
+shoppingList.itemsToBuy = [
+	{name:"Milk", quantity:"2 bottles"},
+	{name:"Cookies", quantity:"2 bags"},
+	{name:"Bread", quantity:"5 pieces"},
+	{name:"Candies", quantity:"5 bags"},
+	{name:"Ice Cream", quantity: "10 bags"},
+];
 
-//method to return all items to buy
 shoppingList.getAllItemsToBuy = function(){
-// console.log(shoppingList.itemsToBuy);
 return shoppingList.itemsToBuy;
 }
 
-//list of bought items
 shoppingList.itemsBought = [];
 
-//method to return all items to buy
 shoppingList.getAllBoughtItems = function(){
-// console.log(shoppingList.itemsToBuy);
 return shoppingList.itemsBought;
 }
 
-//returns number of bought items
 shoppingList.allItemsBought = function() { var result = ( shoppingList.itemsToBuy.length === 0) ? true : false;
 return result;
   }
 
-//returns true if boughtItems list is empty
+
 shoppingList.nothingBought = function() { var result = ( shoppingList.itemsBought.length === 0) ? true : false;
 return result;
-  }
+}
 
-//method to mark item bought
-shoppingList.markBought = function(ind){
-//This will return array with one object (bought item), so we must address it using array index [0]
-var boughtItem = shoppingList.itemsToBuy.splice(ind,1)[0];
-// console.log(boughtItem);
+shoppingList.markBought = function(index){
+
+var boughtItem = shoppingList.itemsToBuy.splice(index,1)[0];
 shoppingList.itemsBought.push(boughtItem);
 console.log(shoppingList.allItemsBought());
 
